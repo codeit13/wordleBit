4a9a1c2dcf2ca539e56fecc1f22e8bc671003a11
Sumit <timus@Sumits-MacBook-Pro.local>
2022-10-30 19:19:35 +0530

    Added wordlIst Api words

--- api/wordsList.js
+++ api/wordsList.js
@@ -1,8 +1,9 @@
+import { allWords, wordsAcceptedAsInput } from "./constants/";
+
 export default function handler(request, response) {
-  console.log("WORDLIST API");
   response.status(200).json({
     body: request.body,
-    query: request.query,
-    cookies: request.cookies,
+    allWords: allWords,
+    wordsAcceptedAsInput: wordsAcceptedAsInput,
   });
 }
